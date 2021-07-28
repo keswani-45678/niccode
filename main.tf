f77f8721d81bae6c3097c056422ff93191c20c08
keswani-45678 <87806290+keswani-45678@users.noreply.github.com>
2021-07-28 11:40:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,5 +12,5 @@ tags = [{"deployment-environment" = "QA", "env"="dev"}]
 resource "azurerm_resource_group" "test1" {
   name     = "test1"
   location = "West Europe"
-  tags = concat(local.tags,var.tagsvalue)
+  tags = ["${concat(local.tags,var.tagsvalue)}"]
 }
