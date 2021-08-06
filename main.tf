dbd5b30655b61a16cfb061a402f61c5e35bc8715
keswani-45678 <87806290+keswani-45678@users.noreply.github.com>
2021-08-06 16:37:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,13 +4,9 @@ provider "azurerm" {
 
  
 
-locals{
-tags = {"deployment-environment" = "QA", "env"="dev"}
-}
-
 
 resource "azurerm_resource_group" "test1" {
   name     = "test1"
   location = "West Europe"
-  tags = merge(local.tags,var.tagsvalue)
+ 
 }
